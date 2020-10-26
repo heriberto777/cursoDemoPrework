5a3dddec74f698ee3d49eabd672cdd2e432f0cb9
Heriberto Gonzalez <heriberto777@gmail.com>
2020-10-25 22:12:32 -0400

    Practica de JavaScript Basico

---
+++ valores.js
@@ -0,0 +1,29 @@
+
+var nombre = "Oscar";
+
+var edad;
+
+edad = 30;
+
+edad
+
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
+
+persona;
+
+//Declarativas
+
+function miFuction() {
+    return 3;
+}
+
+//Expresion
+
+var miFuction = function(){
+    
+}

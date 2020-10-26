a2892588746b2b34df919b13d9b73adee84c04fc
heriberto777 <heriberto777@gmail.com>
2020-10-26 18:12:43 -0400

    Practica JS basico

--- valores.js
+++ valores.js
@@ -27,3 +27,24 @@ function miFuction() {
 var miFuction = function(){
     
 }
+
+
+var op1 = "Piedra";
+var op2 = "Papel";
+var op3 = "Tijera";
+
+var resultado = function(user, cpu){
+    if(user != cpu) {
+      if((user === op1 && cpu === op3) ||
+      (user === op2 && cpu === op1) || 
+      (user === op3 && cpu === op2)) 
+      {
+          console.log("Usuario Gano")
+      } else {
+          console.log("Gano la computadora")
+      }
+    }
+}
+
+var frutas = ["Manzana","Platano","Cereza","Fresa"];
+console.log(frutas);

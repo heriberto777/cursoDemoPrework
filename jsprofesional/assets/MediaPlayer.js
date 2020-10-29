ab67a4ae2c10da3cc50767bc393b4cc2cd87f489
heriberto777 <heriberto777@gmail.com>
2020-10-29 18:00:51 -0400

    JS Profesional

---
+++ jsprofesional/assets/MediaPlayer.js
@@ -0,0 +1,51 @@
+        
+
+        function MediaPlayer(config) {
+            this.media = config.el;
+            this.plugins = config.plugins || [];
+
+            this._initPlugins();
+        }
+
+
+        MediaPlayer.prototype._initPlugins = function() {
+            this.plugins.forEach(plugin => {
+                plugin.run(this);
+            })
+        }
+
+        MediaPlayer.prototype.play = function()  {
+            this.media.play();
+        }
+
+          MediaPlayer.prototype.pause = function() {
+            this.media.pause();
+        }
+
+        MediaPlayer.prototype.togglePlay = function()  {
+               if(this.media.paused) {
+                this.play();
+            } else {
+                this.pause();
+            }
+        }
+
+        MediaPlayer.prototype.mute = function() {
+            this.media.muted  = true
+        }
+
+          MediaPlayer.prototype.unmute = function() {
+            this.media.muted = false;
+        }
+
+
+        MediaPlayer.prototype.unMuteMute = function()  {
+              if(this.media.muted == true){
+                  this.unmute();
+              } else {
+                  this.mute();
+              }
+        }
+
+
+        export default MediaPlayer;
---
+++ jsprofesional/assets/index.js
@@ -0,0 +1,16 @@
+import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js'
+
+const video = document.querySelector("video");
+const player = new MediaPlayer({el: video, plugins: [
+    new AutoPlay()
+]});
+
+const buttonPlay = document.querySelector("#playPause");
+buttonPlay.onclick = () => player.togglePlay();
+
+
+const buttonMute = document.querySelector("#muteUnmute");
+buttonMute.onclick = () => player.unMuteMute();
+        
+
